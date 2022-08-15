0ec68a32a8459b35a5064270b8c585b024d3d3c1
alexandrekermarec <alexkermarec@hotmail.fr>
2022-08-15 15:54:40 +0200

    reponsive

--- src/Components/ProtocolDetail.js
+++ src/Components/ProtocolDetail.js
@@ -13,19 +13,29 @@ const ProtocolDetail = ({protocols}) => {
 
     return (
         
-        <div>
+        <>
             <Link to="/"><button className='btn-grad'>Back</button></Link>
-            <div className='flex-center'>
-                <h2>Protocol : {protocol.name}</h2>
+            <div className='details'>
                 <img className='imgdetail' src={protocol.logo}></img>
+                <div className='flex-center'>
+                    <h2>Protocol : {protocol.name}</h2>
+                    </div>
+                <div className='list'>  
+                    <p>Category : {protocol.category}</p>
+                    <p>Total Value Locked : {protocol.tvl}</p>
+                    <p>Url : <a href={protocol.url}>{protocol.name} url</a></p>
+                    <p>MCap : {protocol.mcap}</p>
+                </div>  
+                <p>Blockchains : </p>
+                    <div className='flex blockchains'>
+                    {protocol.chains.map((blockchain)=>(
+                        <p>{blockchain}</p>
+                    ))
+                    }
                 </div>
-            <li>Category : {protocol.category}</li>
-            <li>TVL : {protocol.tvl}</li>
-            <li>Url : <a href={protocol.url}>{protocol.name} url</a></li>
-            <li>MCap : {protocol.mcap}</li>
-            <li>Blockchains : {protocol.chains}</li>
-            <p>Description : {protocol.description}</p>
-        </div>
+                <p>Description : {protocol.description}</p>
+            </div>
+        </>
     )
 }
 
